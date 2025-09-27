482e0607ed75f62b5d144077c783cf2223804abb
JobertCamo <jobertcamo@gmail.com>
2025-09-28 00:42:54 +0800

    first commit

---
+++ database/migrations/2025_09_27_161620_create_attendances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->dateTime('check_in');
+            $table->dateTime('check_out');
+            $table->enum('status', ['present', 'absent', 'late', 'half-day']);
+            $table->decimal('work_hours', 5,2);
+            $table->text('remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
---
+++ database/migrations/2025_09_27_162151_create_leaves_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leaves', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->enum('leave_type', ['vacation', 'sick', 'emergency', 'unpaid','other']);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->int('total_days');
+            $table->enum('status', ['pending', 'approved', 'rejected', 'canceled'])->default('pending');
+            $table->text('reason')->nullable();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leaves');
+    }
+};
